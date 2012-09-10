8e48b0ede694beddce732e0485f1260a3c2552cc
Jonathan Munoz <jonmgomez@hotmail.com>
2012-09-10 10:59:47 -0500

    Modificaciones a los comentarios HTML y CSS

---
+++ Pruebas.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Blog de Prpueba</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory 
+        <link href='http://fonts.googleapis.com/css?family=Gochi+Hand|Shadows+Into+Light+Two' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light+Two' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Qwigley' rel='stylesheet' type='text/css'> -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.6.1.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+
+        <!--        <div id="barra-sup-der"  class="">
+                    <a href="http://www.twitter.com/jonathanmg7" title ="twitter:@JonathanMG7" ><img src="img/twr.png" class="img-sup" alt="twitter"/></a>
+                    <img src="img/site1.png" class="img-sup" alt="site1"/>
+                    <img src="img/site2.png" class="img-sup" alt="site2"/>
+                    <img src="img/site3.png" class="img-sup" alt="site3"/>
+                    <img src="img/site4.png" class="img-sup" alt="site4"/>
+        
+                </div>-->
+        <div id="barra-sup-bot" class="">
+            <div id="menu-superior">
+                <ul> 
+                    <li><a href="index.html">HOME</a></li>
+                    <li><a href="post.html">POST</a></li>
+                </ul>
+            </div>
+        </div>
+        <div id="cabecera"  class="">
+            <img src="img/Logo.png" class="img-header" alt="Logo" />
+            <div id="header-box-text" class="">
+                <h1>Mi Blog</h1>
+                <h4 class="">Este es el primer blog de prueba desarrollado por <a href="http://twitter.com/JonathanMG7" target="_blank">@JonathanMG7</a></h4>
+            </div>
+        </div>
+        <div id="container" class="b">
+            <p>hola hola hola hola hola hola</p>
+            <div id="w-prueba">
+                
+            </div>
+            <div class="w-prueba2">
+                </div>
+        </div>
+        <footer  >
+            Desarrollado por <a href="http://twitter.com/JonathanMG7" target="_blank">@JonathanMG7</a><br />
+        </footer>
+
+        <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>-->
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.0.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. 
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>-->
+    </body>
+</html>
