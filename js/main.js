ca0f7618690886098eda7eeb47dc634af9fcf270
Jonathan <jonmgomez@hotmail.com>
2012-09-11 15:53:04 -0500

    Comentarios 95% completados

--- js/main.js
+++ js/main.js
@@ -3,20 +3,49 @@ $(document).on("ready" , main)  //Para que el JS solo carge luego que
 
 function main(){                //lista de funciones que estan en el codigo
     closeOnOver()
+    dialogBox()
 }
 
 function closeOnOver(){
     $(".coment_body").hover(function(){                 //Funcion para aparecer
-        $(this).find("i.closer").removeClass("hidden")  //la X al poner el puntero
+        $(this).find("a.closer").removeClass("hidden")  //la X al poner el puntero
     },function(){                                       //sobre el comentario
-        $(this).find("i.closer").addClass("hidden")
+        $(this).find("a.closer").addClass("hidden")
     })
     
     
-    $("#bc1").click(function(){                         //eliminar cada comentario
-        $("#cc1").addClass("hidden");                   //de manera individual
-    });                                                 //gracias a un ID 
-    $("#bc2").click(function(){
-        $("#cc2").addClass("hidden");
-    });
+    $(".closer").click(function(e){                        
+        e.preventDefault();                             //previene el enlace
+        if(confirm("Estas seguro que deseas eliminar")){
+            $(this).parent().parent().parent().fadeOut(500)
+        }
+    }); 
 }
+
+
+function dialogBox() {
+    // a workaround for a flaw in the demo system (http://dev.jqueryui.com/ticket/4375), ignore!
+    $( "#dialog:ui-dialog" ).dialog( "destroy" );
+	
+    $( "#dialog-confirm" ).dialog({
+//        autoOpen: false,
+        resizable: false,
+        height:140,
+        modal: true,
+        buttons: {
+            "Delete all items": function() {
+                $( this ).dialog( "close" );
+            },
+            Cancel: function() {
+                $( this ).dialog( "close" );
+            }
+        }
+    });
+    
+    $( "#opener" ).click(function(e) {
+         e.preventDefault();   
+			$( "#dialog" ).dialog( "open" );
+			return false;
+		});
+}
+	
